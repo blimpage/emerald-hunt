a8bc87d9b837187734b3c9fec93acb1d9dfcc1a7
Pat McKenna <pat@mckenna.rocks>
2017-01-10 19:17:52 +1100

    Implement basic player movement

--- hunt.rb
+++ hunt.rb
@@ -9,26 +9,80 @@ TILE_SIZE = 40
 class EmeraldHunt < Gosu::Window
   def initialize
     super(TILES_X * TILE_SIZE + TILE_SIZE * 2, TILES_Y * TILE_SIZE + TILE_SIZE * 2)
-    
+
     self.caption = "Emerald Hunt"
-    
+
     @matrix = Array.new(TILES_Y, Array.new(TILES_X))
+    @player = Player.new
 
     @font = Gosu::Font.new(10)
   end
-  
+
   def update
-    
+    @player.update
   end
-  
+
   def draw
     @matrix.each_with_index do |row, y_index|
       row.each_with_index do |tile, x_index|
         x_position = TILE_SIZE + x_index * TILE_SIZE
         y_position = TILE_SIZE + y_index * TILE_SIZE
-        @font.draw("#{x_index}, #{y_index}", x_position, y_position, 0)
+
+        if @player.x == x_index && @player.y == y_index
+          @font.draw("@@@", x_position, y_position, 1)
+        else
+          @font.draw("#{x_index}, #{y_index}", x_position, y_position, 0, 1, 1, 0xff_888888)
+        end
       end
     end
+  end
+end
+
+class Player
+  attr_reader :x, :y
+
+  MINIMUM_MOVE_TIME = 150
+
+  def initialize
+    @x = (TILES_X / 2).floor
+    @y = (TILES_Y / 2).floor
+    @last_move_time = Gosu.milliseconds
+  end
+
+  def update
+    if can_move_now?
+      if Gosu::button_down?(Gosu::KbLeft)
+        if @x > 0
+          @x = @x - 1
+          @last_move_time = Gosu.milliseconds
+        end
+      elsif Gosu::button_down?(Gosu::KbRight)
+        if @x < TILES_X - 1
+          @x = @x + 1
+          @last_move_time = Gosu.milliseconds
+        end
+      elsif Gosu::button_down?(Gosu::KbUp)
+        if @y > 0
+          @y = @y - 1
+          @last_move_time = Gosu.milliseconds
+        end
+      elsif Gosu::button_down?(Gosu::KbDown)
+        if @y < TILES_Y - 1
+          @y = @y + 1
+          @last_move_time = Gosu.milliseconds
+        end
+      end
+    end
+  end
+
+  private
+
+  def last_move_delta
+    Gosu.milliseconds - @last_move_time
+  end
+
+  def can_move_now?
+    last_move_delta >= MINIMUM_MOVE_TIME
   end
 end
 
