1c3e1bf2449b83d84f8b467834659aeadc9e1e31
Pat McKenna <pat@mckenna.rocks>
2017-01-10 20:01:52 +1100

    Implement Tile class

--- hunt.rb
+++ hunt.rb
@@ -16,10 +16,20 @@ class EmeraldHunt < Gosu::Window
     @player = Player.new
 
     @font = Gosu::Font.new(10)
+
+    @last_debug_dump = Gosu.milliseconds
   end
 
   def update
+    @board.free_tile(@player.x, @player.y)
     @player.update
+    @board.set_tile_contents(@player.x, @player.y, @player)
+
+    if Gosu::button_down?(Gosu::KbD) && Gosu.milliseconds - @last_debug_dump > 1000
+      @last_debug_dump = Gosu.milliseconds
+      puts "\n\n\n"
+      puts @board.inspect
+    end
   end
 
   def draw
@@ -27,10 +37,13 @@ class EmeraldHunt < Gosu::Window
       x_position = TILE_SIZE + x_index * TILE_SIZE
       y_position = TILE_SIZE + y_index * TILE_SIZE
 
-      if @player.x == x_index && @player.y == y_index
+      case tile.object_type
+      when :player
         @font.draw("@@@", x_position, y_position, 1)
-      else
+      when :null_object
         @font.draw("#{x_index}, #{y_index}", x_position, y_position, 0, 1, 1, 0xff_888888)
+      else
+        @font.draw("???", x_position, y_position, 0, 1, 1, 0xff_ff0000)
       end
     end
   end
@@ -38,7 +51,12 @@ end
 
 class Board
   def initialize
-    @matrix = Array.new(TILES_Y, Array.new(TILES_X))
+    @null_object = NullObject.new
+    @matrix = Array.new(TILES_Y) do
+      Array.new(TILES_X) do
+        Tile.new(@null_object)
+      end
+    end
   end
 
   def each_tile(&block)
@@ -47,6 +65,41 @@ class Board
         yield(tile, x_index, y_index)
       end
     end
+  end
+
+  def set_tile_contents(x_index, y_index, contents)
+    tile_at(x_index, y_index).set_contents(contents)
+  end
+
+  def free_tile(x_index, y_index)
+    tile_at(x_index, y_index).set_contents(@null_object)
+  end
+
+  def tile_at(x_index, y_index)
+    @matrix[y_index][x_index]
+  end
+end
+
+class Tile
+  def initialize(contents)
+    @contents = contents
+  end
+
+  def set_contents(contents)
+    @contents = contents
+  end
+
+  def object_type
+    @contents.object_type
+  end
+end
+
+class NullObject
+  def initialize
+  end
+
+  def object_type
+    :null_object
   end
 end
 
@@ -85,6 +138,10 @@ class Player
         end
       end
     end
+  end
+
+  def object_type
+    :player
   end
 
   private
